f0a2a0f6f73317c1194e35ec63ad510cd854d2b0
Rebekah <figueroarebekah@gmail.com>
2022-12-14 10:50:03 -0600

    commit tree 7451f1ec27aed2c9ed4fabfe0e3e4506ff9501c7

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const num3 = 19;
+const num4 = 5;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random());
+const mod = 19 % 5;
+const max = Math.max(2, 4, 7, 9, 15, 17, 20);
